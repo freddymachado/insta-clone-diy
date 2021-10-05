30da547d9bf859fc8c337ab47c8f854aa215796b
Freddy Machado <freddyantoniomachado@gmail.com>
2021-10-05 17:27:19 -0400

    Debugging

--- app/src/main/java/com/gvm/diy/fragments/UploadFragment.java
+++ app/src/main/java/com/gvm/diy/fragments/UploadFragment.java
@@ -8,8 +8,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.gvm.diy.R;
+import com.makeramen.roundedimageview.RoundedImageView;
 
 public class UploadFragment extends Fragment {
     Uri resultUri;
@@ -40,6 +43,34 @@ public class UploadFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload, container, false);
+        View itemView = inflater.inflate(R.layout.fragment_search, container, false);
+
+        TextView textViewPublish, textViewName, textViewUser, textViewDescription;
+        ImageButton imageButtonBack;
+        RoundedImageView roundedImageViewAvatar;
+
+        textViewPublish = itemView.findViewById(R.id.textViewPublish);
+        textViewName = itemView.findViewById(R.id.textViewName);
+        textViewUser = itemView.findViewById(R.id.textViewUser);
+
+        imageButtonBack = itemView.findViewById(R.id.imageButtonBack);
+        roundedImageViewAvatar = itemView.findViewById(R.id.user_profile_image);
+        textViewDescription = itemView.findViewById(R.id.textViewDescription);
+
+        roundedImageViewAvatar.setImageURI(resultUri);
+        imageButtonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        textViewPublish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return itemView;
     }
 }
